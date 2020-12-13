794d33af20be72361f62873ba8c7c4b39111a596
poengAlex <41318737+poengAlex@users.noreply.github.com>
2020-12-13 10:36:53 +0100

    Removed https test keys

--- server.js
+++ server.js
@@ -25,20 +25,3 @@ app.use(express.static(__dirname + "/public/"));
 http.listen(PORT, function () {
     console.log("listening on PORT:" + PORT);
 });
-
-https
-    .createServer(
-        {
-            key: fs.readFileSync("63040593_192.168.137.1.key"),
-            cert: fs.readFileSync("63040593_192.168.137.1.cert"),
-        },
-        app
-    )
-    .listen(PORT_HTTPS, function () {
-        console.log(
-            "Example app listening on port (HTTPS)" +
-            PORT_HTTPS +
-            "! Go to https://localhost:" +
-            PORT_HTTPS
-        );
-    });
